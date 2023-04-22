fbea4f582476c9b901f6ecc38a898f80d67fa3ae
VED <46843546+awesome-ved@users.noreply.github.com>
2023-04-22 04:51:51 +0000

    ii commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import profileLogo from './Ved_Vadake_Photo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+/* <div className="App">
+      <header className="App-header">
+        <img src={profileLogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,8 +30,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+    </div> */
