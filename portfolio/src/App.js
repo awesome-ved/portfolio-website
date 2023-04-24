4fea75279f7e77c9d31697839632f5a52c2d3322
Ved.Vadake <Ved.Vadake@bentley.com>
2023-04-24 23:16:29 +0530

    iv commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,48 @@
 // import logo from './logo.svg';
 import profileLogo from './Ved_Vadake_Photo.jpeg';
+// import resume from './Ved_Vadake_Resume.pdf';
 import './App.css';
 
+const PDF_URL = "./Ved_Vadake_Resume.pdf";
+
 function App() {
+  const downloadFileAtURL=(url)=>{
+    const fileName = url.split('/').pop();
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download",fileName);
+    document.body.appendChild(aTag) ;
+    aTag.click();
+    aTag.remove();
+  }
   return (
     <div className="App">
+      <div className="header">
+        <p>Portfolio</p>
+      </div>
+      <div className="midcontent">
+        <div className="profile">
+          <img src={profileLogo} className="App.logo" alt="logo"></img>
+          <h1>Ved Vadake</h1>
+          <button onClick={()=>{downloadFileAtURL(PDF_URL)}}>Save the Resume</button>
+        </div>
+        <div className="pdf"></div>
+      </div>
+      <div className="footer">
+        <p>Visit My <button className="linkedin">LinkedIN</button> or <button className="github">Github</button></p>
+      </div>
+    </div>
+  );
+}
+
+
+
+export default App;
+
+
+/* 
+
+<div className="App">
       <header className="App-header">
         <p>Portfolio Resume</p>
       </header>
@@ -17,15 +55,13 @@ function App() {
         <article>
           <h1>Resume</h1>
         </article>
+        <footer>
+          <p>Footer</p>
+        </footer>
       </section>
     </div>
-  );
-}
 
-export default App;
-
-
-/* <div className="App">
+<div className="App">
       <header className="App-header">
         <img src={profileLogo} className="App-logo" alt="logo" />
         <p>
